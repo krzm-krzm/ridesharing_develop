ee0f91c30feeb0fa8108d3d8cf0ea40e5ba68b1d
kuro <k537056@kansai-u.ac.jp>
2021-06-21 14:55:59 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import numpy as np
+import random
+import sys
+
+mat =[]
+FILENAME='darp01.txt'
+with open('/home/kurozumi/デスクトップ/benchmark/'+ FILENAME,'r',encoding='utf-8') as fin:
+    for line in fin.readlines():
+        row = []
+        toks = line.split(' ')
+        for tok in toks:
+            try:
+                num = float(tok)
+            except ValueError:
+                continue
+            row.append(num)
+        mat.append(row)
+print(mat)
+
+Setting_Info = mat.pop(0)
+print(Setting_Info)
+print(mat)
